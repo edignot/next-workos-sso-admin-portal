9defc9e23130f476ab1eb3b7cdd78f28295f5600
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-06-26 18:22:05 -0600

    Env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    WORKOS_API_KEY:
+      'sk_test_a2V5XzAxRzI1WkhWTUhKOVJZNDZXUTFTUzIyOTZXLGZIVU9mYTdsaUVlTVlLZzR3cXFBMjgwQTA',
+    WORKOS_CLIENT_ID: 'client_01G25ZHVMQ1P8EY19JZZG0ZYQ9',
+  },
 }
 
 module.exports = nextConfig
